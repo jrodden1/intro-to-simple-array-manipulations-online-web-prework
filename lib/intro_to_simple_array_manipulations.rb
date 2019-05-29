36b3ab0b04c09d1f735310932e9136b3ed357ef8
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 18:29:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,11 @@ end
 def pop_with_args(array)
   chars_arya_killed = array.pop(2)
 end
+
+def using_shift(array)
+  im_so_over_this_city = array.shift
+end
+
+def shift_with_args(array)
+  brands_removed = array.shift(2)
+end
