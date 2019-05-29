9c7affeb75b8fbe684defabfe91a1dc28deb5113
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 18:27:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(country_array, country)
   updated_array = country_array.push country
 end
+
+def using_unshift(array, string)
+  updated_array = array.unshift string
+end
+
+def using_pop(array)
+  deleted_string = array.pop  
+end
+
+def pop_with_args(array)
+  chars_arya_killed = array.pop(2)
+end
